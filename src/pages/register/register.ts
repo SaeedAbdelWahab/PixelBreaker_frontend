0101bbffc3ee937056e1be34dc0c195223567622
SaeedAbdelWahab <saeed.abdellwahab@gmail.com>
2018-04-07 19:58:02 +0200

    add register front and back

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AccountsProvider } from '../../providers/accounts/accounts';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the RegisterPage page.
@@ -14,12 +17,81 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  data ;
+  messages:any;
+  message_body:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public accountsProvider:AccountsProvider,public alertCtrl:AlertController) {
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  register(data){
+    this.data = data;
+    this.accountsProvider.register(JSON.stringify(data.value)).subscribe(res=>{
+      this.createAlert("Congrats your account was created!");
+
+    },
+    err => {
+      this.messages = {
+        "email":err.error.email,
+        "password":err.error.password
+      }
+      if (this.messages.email && this.messages.password){
+        
+      }
+      else{
+        if (this.messages.email){
+          this.messages = {
+            "password":"No errors \n",
+            "email":err.error.email+"\\\\",
+          }
+        }
+        else{
+          this.messages = {
+            "email":"No errors \n. \\\\",
+            "password":err.error.password,
+          }
+        }
+
+      }
+      this.showAlert(this.messages);
+    
+
+    }
+  );
+  }
+  showAlert(message) {
+    
+       
+    let alert = this.alertCtrl.create({
+      title: 'Error!',
+      
+      message:"Email : "+message.email+"     "+ "Password : "+message.password,
+  
+      buttons: ['OK']
+
+    });
+    alert.present();
+  }
+
+  createAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Congrats!',
+      subTitle: message,
+      buttons: [{
+        text: "Ok",
+        handler :()=>{
+          this.navCtrl.push(LoginPage,{
+            email : this.data.value.email,
+            password : this.data.value.password
+          })
+        }
+      }]
+    });
+    alert.present();
   }
 
 }
