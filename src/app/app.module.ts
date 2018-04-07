c13fc5739945058e9460b17171e2c0b32681b6db
SaeedAbdelWahab <saeed.abdellwahab@gmail.com>
2018-04-07 19:14:59 +0200

    add register page and adjust nav in startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { StartupPage } from '../pages/startup/startup';
 import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     StartupPage,
-    LoginPage
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
@@ -35,7 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     StartupPage,
-    LoginPage
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
--- src/pages/startup/startup.ts
+++ src/pages/startup/startup.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage} from '../login/login';
+import { RegisterPage } from '../register/register'
+
 
 /**
  * Generated class for the StartupPage page.
@@ -20,6 +23,13 @@ export class StartupPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StartupPage');
+  }
+
+  goToSignup(){
+    this.navCtrl.push(RegisterPage);
+  }
+  goToSignIn(){
+    this.navCtrl.push(LoginPage);
   }
 
 }
